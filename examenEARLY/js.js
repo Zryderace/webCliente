75008dc477fc9d5a3400449ba9098d3f80468a12
zryderace <alemoled@gmail.com>
2024-10-15 19:35:15 +0200

    practica

---
+++ examenEARLY/js.js
@@ -0,0 +1,65 @@
+
+function ej1() {
+    let valor = -1;
+    let dollar = 1.0965;
+    do {
+        valor = parseFloat(prompt(`introduce un valor para convertir a euros, mayor a 0`));
+    } while (valor <= 0 || isNaN(valor));
+    alert(`${valor} euros son ${valor*dollar} dolares`)
+}
+
+function ej2(){
+    let notas = [];
+    let valor;
+    let suspensos = 0, aprobados = 0, notables = 0, sobresalientes = 0;
+    let i = 0;
+    do {
+        do {
+            valor = -1;
+            valor = parseFloat(prompt(`introduce la calificacion, mayor a 0 y menor a 10`));
+        } while (valor < 0 || isNaN(valor) || valor>10);
+        if (valor<5) {
+            suspensos++;
+        } else if (valor<7) {
+            aprobados++;
+        } else if (valor<9) {
+            notables++;
+        } else {
+            sobresalientes++;
+        }
+        i++;
+    } while (i<10);
+    alert(`
+         ${suspensos} suspensos
+         ${aprobados} aprobados
+         ${notables} notables
+         ${sobresalientes} sobresalientes
+         `)
+}
+
+function ej3(){
+    let arr = [];
+    let valor;
+    //a
+    do {
+        valor = "";
+        valor = parseFloat(prompt(`mete numeros, acaba con un negativo`));
+        if (valor>=0) {
+            arr.push(valor);
+        }
+    } while (valor>=0||isNaN(valor));
+    //b
+    let str = "";
+    for (let i = 0; i < arr.length; i++) {
+        str+= `${arr[i]} `;
+    }
+    console.log(str);
+}
+
+// let a = parseFloat(prompt(`introduce un valor`));
+// let b = parseFloat(prompt(`introduce un valor`));
+// alert("suma:" + suma(a, b) +
+//     "resta:" + resta(a, b) +
+//     "multiplicacion:" + multiplicacion(a, b) +
+//     "division:" + division(a, b) +
+//     "resto:" + resto(a, b))
